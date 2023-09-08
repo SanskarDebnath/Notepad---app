d81400542c632a740956e3b2077780b09ffa4709
Sanskar Debnath <104642711+SanskarDebnath@users.noreply.github.com>
2023-09-08 22:32:19 +0530

    Add files via upload

---
+++ notepad.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+
+def newFile():
+    global file
+    root.title("Untitled - Notepad")
+    file = None #to reset the variable after Newfile()
+    TextArea.delete(1.0, END) #arguments first to last
+
+def openFile():
+    global file
+    file=askopenfilename(defaultextension=".txt", filetypes = [("All Files", "*.*"), ("Text Documents", "*.txt")])
+    if file == "":
+        file=None
+    else:
+        root.title(os.path.basename(file)+" - Notepad")
+        TextArea.delete(1.0, END)
+        f = open(file, "r")
+        TextArea.insert(1.0, f.read())
+        f.close()
+
+def saveFile():
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile='untitled.txt', defaultextension=".txt", filetypes = [("All Files", "*.*"), ("Text Documents", "*.txt")])
+        if file == "":
+            file=None
+
+        #save as a new file
+        else:
+            f= open(file, "w")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+            root.title(os.path.basename(file) + " - Notepad")
+            print("file saved")
+    else:
+        f= open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
+
+def quitApp():
+    root.destroy()
+
+def cut():
+    TextArea.event_generate(("<<Cut>>"))
+
+def copy():
+    TextArea.event_generate(("<<Copy>>"))
+
+def paste():
+    TextArea.event_generate(("<<Paste>>"))
+
+def about():
+    showinfo("Notepad V-1.0", "This Notepad was Developed by sanskar debnath using Tkinter")
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Untitled File")
+    root.geometry("644x788")
+
+    #text area
+    TextArea = Text (root, font="lucida 13")
+    file=None
+    TextArea.pack(expand = True, fill=BOTH)
+    #menu bar
+    MenuBar = Menu(root)
+    FileMenu = Menu (MenuBar, tearoff=0)
+    #open new file
+    FileMenu.add_command(label="New", command=newFile)
+    #to open a file
+    FileMenu.add_command(label="open", command=openFile)
+    #to save a file
+    FileMenu.add_command(label = "Save", command=saveFile)
+    FileMenu.add_separator()
+    FileMenu.add_command(label = "Exit", command = quitApp)
+    MenuBar.add_cascade(label = "File", menu = FileMenu)
+    #file menu starts
+    #edit menu starts
+    EditMenu = Menu(MenuBar, tearoff=0)
+    #cut, copy & paste feature
+    EditMenu.add_command(label="Cut", command=cut)
+    EditMenu.add_command(label="Copy", command=copy)
+    EditMenu.add_command(label="Paste", command=paste)
+    MenuBar.add_cascade(label="Edit", menu=EditMenu)
+    #edit menu Ends
+    #help menu starts 
+    HelpMenu = Menu(MenuBar, tearoff=0)
+    HelpMenu.add_command(label="About this App", command=about)
+    MenuBar.add_cascade(label="Help", menu=HelpMenu)
+    #help menu ends
+    #file menu Ends
+    root.config(menu=MenuBar)
+    #Adding Scrollbar:
+    Scroll = Scrollbar(TextArea)
+    Scroll.pack(side=RIGHT, fill=Y)
+    Scroll.config(command=TextArea.yview)
+    TextArea.config(yscrollcommand=Scroll.set)
+    root.mainloop()
